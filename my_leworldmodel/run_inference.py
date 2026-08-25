daa84ef980095245a4a33ce1081510213801452a
perchik_air <1919ars@gmail.com>
2026-08-25 11:01:27 +0400

    more

--- my_leworldmodel/run_inference.py
+++ my_leworldmodel/run_inference.py
@@ -13,9 +13,10 @@ def get_closest_next_state(encoder, curr_state, goal_state):
         next_state = create_next_state(curr_state, action)
         next_state_emb = encoder(next_state.unsqueeze(0))
         goal_state_emb = encoder(goal_state.unsqueeze(0))
-        distance = F.cosine_similarity(next_state_emb, goal_state_emb, dim=-1)
+        # distance = F.cosine_similarity(next_state_emb, goal_state_emb, dim=-1)
+        distance = (next_state_emb - goal_state_emb).norm(p=2)
         distance_list.append((action, next_state, distance))
-    action, next_state, distance = min(distance_list, key=lambda t: t[2])
+    action, next_state, distance = max(distance_list, key=lambda t: t[2])
     return action, next_state, distance
 
 
@@ -29,27 +30,27 @@ def main():
     predictor = Predictor(out_features=out_features)
     predictor.load_state_dict(torch.load('predictor.pt'))
 
-
-    curr_state = create_curr_state(side=side)
-    plt.imshow(curr_state.squeeze().detach().numpy())
-    plt.show()
-    goal_state = create_curr_state(side=side)
-    plt.imshow(goal_state.squeeze().detach().numpy())
-    plt.show()
-
-    visited_states = [curr_state]
-    for i in range(100):
-        action, next_state, distance = get_closest_next_state(encoder, curr_state, goal_state)
-        visited_states.append(next_state)
-
-        plt.cla()
+    for i_try in range(10):
+        curr_state = create_curr_state(side=side)
         plt.imshow(curr_state.squeeze().detach().numpy())
-        plt.title(f'iter: {i}')
-        plt.pause(0.001)
+        plt.show()
+        goal_state = create_curr_state(side=side)
+        plt.imshow(goal_state.squeeze().detach().numpy())
+        plt.show()
 
-        curr_state = next_state
+        visited_states = [curr_state]
+        for i in range(20):
+            action, next_state, distance = get_closest_next_state(encoder, curr_state, goal_state)
+            visited_states.append(next_state)
 
-    plt.show()
+            plt.cla()
+            plt.imshow(curr_state.squeeze().detach().numpy())
+            plt.title(f'iter: {i} | action: {action} | distance: {distance.item()}')
+            plt.pause(0.1)
+
+            curr_state = next_state
+
+        plt.show()
 
     # data = get_nav_data(N=N, side=side)
     # for curr_state, rand_action, next_state in data:
